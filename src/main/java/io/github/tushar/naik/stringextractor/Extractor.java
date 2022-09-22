16a3d270638567043ca9598417893abcdcacb5e6
Tushar Naik <tushar.knaik@gmail.com>
2022-09-22 19:09:34 +0530

    added a default impl in Extractor without contextmap

--- src/main/java/io/github/tushar/naik/stringextractor/Extractor.java
+++ src/main/java/io/github/tushar/naik/stringextractor/Extractor.java
@@ -1,7 +1,11 @@
 package io.github.tushar.naik.stringextractor;
 
+import java.util.Collections;
 import java.util.Map;
 
 public interface Extractor {
+    default ExtractionResult extractFrom(String source) {
+        return extractFrom(source, Collections.emptyMap());
+    }
     ExtractionResult extractFrom(String source, Map<String, String> contextMap);
 }
